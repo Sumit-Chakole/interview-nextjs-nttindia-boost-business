3bdc1df708fb52eeec77953725384bf508d6bc79
prayash-c <prayash.chettri@tarkashilpa.com>
2025-11-05 10:04:35 +0530

    Problem Statement
    - Images and necessary logos have been added
    
    - Content is also provided

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
-import './globals.scss';
-import type { Metadata } from 'next';
-import { Noto_Serif } from 'next/font/google';
-
-const notoSerif = Noto_Serif({ subsets: ['latin'] });
+import "./globals.scss";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'NTT Data',
-  description: 'NTT Data is a leading IT consulting firm',
+  title: "NTT Data",
+  description: "NTT Data is a leading IT consulting firm",
 };
 
 export default function RootLayout({
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={notoSerif.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
